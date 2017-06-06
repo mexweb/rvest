824488c656c2d8fdf44b8d1b241c4f10838ce0aa
Bertoldi <massimo.bertoldi@unitn.it>
2017-06-06 15:23:13 +0200

    Modifica funzione is_submit per farlo funzionare in caso di type NULL

--- R/form.R
+++ R/form.R
@@ -277,7 +277,9 @@ submit_form <- function(session, form, submit = NULL, ...) {
 }
 
 submit_request <- function(form, submit = NULL) {
-  is_submit <- function(x) tolower(x$type) %in% c("submit", "image", "button")
+  is_submit <- function(x) ifelse(is.null(x$type),
+                                  FALSE,
+                                  tolower(x$type) %in% c("submit", "image", "button"))
 
   submits <- Filter(is_submit, form$fields)
   if (length(submits) == 0) {
