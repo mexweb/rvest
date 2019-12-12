c44a1d6902604857ecf72119aabbdc701122866b
Bertoldi <massimo.bertoldi@unitn.it>
2019-12-12 13:24:48 +0100

    rebase

--- R/selectors.R
+++ R/selectors.R
@@ -1,48 +1,52 @@
 #' Select nodes from an HTML document
 #'
-#' More easily extract pieces out of HTML documents using XPath and css
+#' More easily extract pieces out of HTML documents using XPath and CSS
 #' selectors. CSS selectors are particularly useful in conjunction with
-#' \url{http://selectorgadget.com/}: it makes it easy to find exactly
-#' which selector you should be using. If you have't used css selectors
+#' <http://selectorgadget.com/>: it makes it easy to find exactly
+#' which selector you should be using. If you haven't used CSS selectors
 #' before, work your way through the fun tutorial at
-#' \url{http://flukeout.github.io/}
+#' <http://flukeout.github.io/>
 #'
-#' @section \code{html_node} vs \code{html_nodes}:
-#' \code{html_node} is like \code{[[} it always extracts exactly one
-#' element. When given a list of nodes, \code{html_node} will always return
-#' a list of the same length, the length of \code{html_nodes} might be longer
+#' @section `html_node` vs `html_nodes`:
+#' `html_node` is like `[[` it always extracts exactly one
+#' element. When given a list of nodes, `html_node` will always return
+#' a list of the same length, the length of `html_nodes` might be longer
 #' or shorter.
 #'
 #' @section CSS selector support:
 #'
 #' CSS selectors are translated to XPath selectors by the \pkg{selectr}
 #' package, which is a port of the python \pkg{cssselect} library,
-#' \url{https://pythonhosted.org/cssselect/}.
+#' <https://pythonhosted.org/cssselect/>.
 #'
 #' It implements the majority of CSS3 selectors, as described in
-#' \url{http://www.w3.org/TR/2011/REC-css3-selectors-20110929/}. The
+#' <http://www.w3.org/TR/2011/REC-css3-selectors-20110929/>. The
 #' exceptions are listed below:
 #'
 #' \itemize{
 #' \item Pseudo selectors that require interactivity are ignored:
-#'   \code{:hover}, \code{:active}, \code{:focus}, \code{:target},
-#'   \code{:visited}
+#'   `:hover`, `:active`, `:focus`, `:target`,
+#'   `:visited`
 #' \item The following pseudo classes don't work with the wild card element, *:
-#'   \code{*:first-of-type}, \code{*:last-of-type}, \code{*:nth-of-type},
-#'   \code{*:nth-last-of-type}, \code{*:only-of-type}
-#' \item It supports \code{:contains(text)}
-#' \item You can use !=, \code{[foo!=bar]} is the same as \code{:not([foo=bar])}
-#' \item \code{:not()} accepts a sequence of simple selectors, not just single
+#'   `*:first-of-type`, `*:last-of-type`, `*:nth-of-type`,
+#'   `*:nth-last-of-type`, `*:only-of-type`
+#' \item It supports `:contains(text)`
+#' \item You can use !=, `[foo!=bar]` is the same as `:not([foo=bar])`
+#' \item `:not()` accepts a sequence of simple selectors, not just single
 #'   simple selector.
 #' }
 #'
 #' @param x Either a document, a node set or a single node.
-#' @param css,xpath Nodes to select. Supply one of \code{css} or \code{xpath}
-#'   depending on whether you want to use a css or xpath 1.0 selector.
+#' @param css,xpath Nodes to select. Supply one of `css` or `xpath`
+#'   depending on whether you want to use a CSS or XPath 1.0 selector.
 #' @export
 #' @examples
 #' # CSS selectors ----------------------------------------------
-#' ateam <- read_html("http://www.boxofficemojo.com/movies/?id=ateam.htm")
+#' url <- paste0(
+#'   "https://web.archive.org/web/20190202054736/",
+#'   "https://www.boxofficemojo.com/movies/?id=ateam.htm"
+#' )
+#' ateam <- read_html(url)
 #' html_nodes(ateam, "center")
 #' html_nodes(ateam, "center font")
 #' html_nodes(ateam, "center font b")
@@ -75,7 +79,7 @@
 #'
 #' # XPath selectors ---------------------------------------------
 #' # chaining with XPath is a little trickier - you may need to vary
-#' # the prefix you're using - // always selects from the root noot
+#' # the prefix you're using - // always selects from the root node
 #' # regardless of where you currently are in the doc
 #' ateam %>%
 #'   html_nodes(xpath = "//center//font//b") %>%
--- cran-comments.md
+++ cran-comments.md
@@ -1,27 +1,14 @@
 ## Test environments
-* local OS X install, R 3.3.0
-* ubuntu 12.04 (on travis-ci), R 3.3.0
-* win-builder (devel and release)
+
+* local: darwin15.6.0-3.5.2
+* travis: 3.1, 3.2, 3.3, oldrel, release, devel
+* r-hub: windows-x86_64-devel, ubuntu-gcc-release, fedora-clang-devel
+* win-builder: windows-x86_64-devel
 
 ## R CMD check results
 
-0 errors | 0 warnings | 2 notes
+0 errors | 0 warnings | 0 notes
 
-* Found the following (possibly) invalid URLs:
-  URL: http://www.crummy.com/software/BeautifulSoup/
-  
-  I see this on win-builder but not locally, so this might be
-  an SSL misconfig (old certs?) on win-builder.
- 
-* checking dependencies in R code ... NOTE
-  Missing or unexported object: 'xml2::xml_find_first'
-  
-  This is a new function in xml2 1.0.0 currently protected behind a
-  packageVersion() switch.
+## revdepcheck results
 
-## Reverse dependencies
-
-I have run R CMD check on all 19 downstream dependencies
-(results at https://github.com/hadley/rvest/blob/master/revdep/).
-
-There were no errors or warnings.
+This release just fixes an example that was failing on CRAN so I did not run the revdeps.
